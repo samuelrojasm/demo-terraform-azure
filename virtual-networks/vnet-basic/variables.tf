088e9978ec3ef44f5aaa48a08e25e602cd8e9693
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-03-21 19:46:07 -0600

    vnet-basic

---
+++ virtual-networks/vnet-basic/variables.tf
@@ -0,0 +1,35 @@
+variable "resource_group_name" {
+  description = "Nombre del grupo de recursos"
+  type        = string
+  default     = "rg-vnet-demo"
+}
+
+variable "location" {
+  description = "Ubicación de la VNet"
+  type        = string
+  default     = "eastus"
+}
+
+variable "vnet_name" {
+  description = "Nombre de la VNet"
+  type        = string
+  default     = "vnet-demo"
+}
+
+variable "vnet_address_space" {
+  description = "Rango de direcciones IP para la VNet"
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "subnet_name" {
+  description = "Nombre de la subred"
+  type        = string
+  default     = "subnet-demo"
+}
+
+variable "subnet_prefix" {
+  description = "Prefijo de la subred"
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
