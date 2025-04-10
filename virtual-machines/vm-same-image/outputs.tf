e9c6b75f941ce796828f4fb6fa5dcb147d71e1ff
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-04-09 23:51:36 -0600

    vm-same-image

---
+++ virtual-machines/vm-same-image/outputs.tf
@@ -0,0 +1,14 @@
+
+
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "private_ip_address" {
+  value = azurerm_windows_virtual_machine.main.private_ip_address
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = azurerm_windows_virtual_machine.main.admin_password
+}
