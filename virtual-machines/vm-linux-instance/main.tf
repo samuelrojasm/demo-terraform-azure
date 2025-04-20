8944ec1b8919a9f246f14f8b3813fb82cb8cddcc
srojas <srojas@192.168.1.15>
2025-04-20 17:20:24 -0600

    vm-linux-instance

--- virtual-machines/vm-linux-instance/main.tf
+++ virtual-machines/vm-linux-instance/main.tf
@@ -48,7 +48,7 @@ resource "azurerm_subnet" "subnet" {
 
 resource "azurerm_public_ip" "public_ip" {
   name                = local.public_ip_name
-  location            = zurerm_resource_group.rg.location
+  location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Basic"
