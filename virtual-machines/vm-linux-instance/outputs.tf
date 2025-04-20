c0499d6a4883d1f12f039aad89d87914fbbbb268
srojas <srojas@192.168.1.15>
2025-04-20 07:57:30 -0600

    vm-linux-instance

---
+++ virtual-machines/vm-linux-instance/outputs.tf
@@ -0,0 +1,12 @@
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "private_ip_address" {
+  value = azurerm_linux_virtual_machine.vm.private_ip_address
+}
+
+output "admin_password" {
+  sensitive = true
+  value     = azurerm_linux_virtual_machine.vm.admin_password
+}
