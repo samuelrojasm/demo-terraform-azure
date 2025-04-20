bb66014348861c558182e44159dd59e02dc97a57
srojas <srojas@192.168.1.15>
2025-04-20 11:51:56 -0600

    vm-linux-instance

--- virtual-machines/vm-linux-instance/outputs.tf
+++ virtual-machines/vm-linux-instance/outputs.tf
@@ -11,16 +11,14 @@ output "vm_info" {
 
     # Red
     private_ip = azurerm_network_interface.nic.private_ip_address
-    public_ip  = try(azurerm_public_ip.vm_public_ip.ip_address, null) # Solo si usas IP pública
     subnet_id  = azurerm_network_interface.nic.ip_configuration[0].subnet_id
-    vnet_name  = try(data.azurerm_virtual_network.vnet.name, null) # Si usas un data source
+    vnet_name  = try(azurerm_virtual_network.vnet.name, null) # Si usas un data source
 
     # Disco
     os_disk_name         = azurerm_linux_virtual_machine.vm.os_disk[0].name
     os_disk_caching      = azurerm_linux_virtual_machine.vm.os_disk[0].caching
     os_disk_size_gb      = azurerm_linux_virtual_machine.vm.os_disk[0].disk_size_gb
     storage_account_type = azurerm_linux_virtual_machine.vm.os_disk[0].storage_account_type
-    data_disks           = try(azurerm_linux_virtual_machine.vm.storage_data_disk, [])
 
     # Managed Identity
     identity_type = azurerm_linux_virtual_machine.vm.identity[0].type # Managed Identity (System Assigned)
@@ -29,19 +27,21 @@ output "vm_info" {
 
     # NSG
     nsg_name  = azurerm_network_security_group.nsg.name
-    nsg_id    = try(azurerm_network_interface.nic.network_security_group_id, null)
-    nsg_rules = try(azurerm_network_security_group.vm_nsg.security_rule, [])
+    nsg_id    = try(azurerm_network_security_group.nsg.id, null)
+    nsg_rules = try(azurerm_network_security_group.nsg.security_rule, [])
 
     # Extensiones
-    aad_ssh_extension_status = try(azurerm_virtual_machine_extension.aadsshlogin.provisioning_state, null)
-    aad_ssh_extension_name   = try(azurerm_virtual_machine_extension.aadsshlogin.name, null)
+    aad_ssh_extension_name      = try(azurerm_virtual_machine_extension.aad_login.name, null)
+    aad_ssh_extension_status    = try(azurerm_virtual_machine_extension.aad_login.id, null)
+    aad_ssh_extension_type      = try(azurerm_virtual_machine_extension.aad_login.type, null)
+    aad_ssh_extension_publisher = try(azurerm_virtual_machine_extension.aad_login.publisher, null)
+    aad_ssh_extension_vm_id     = try(azurerm_virtual_machine_extension.aad_login.virtual_machine_id, null)
 
     # Boot diagnostics
-    boot_diagnostics_enabled = azurerm_linux_virtual_machine.vm.boot_diagnostics[0].enabled
-    boot_diag_storage_uri    = azurerm_linux_virtual_machine.vm.boot_diagnostics[0].storage_account_uri
+    boot_diag_storage_uri = try(azurerm_linux_virtual_machine.vm.boot_diagnostics[0].storage_account_uri, null)
 
     # Admin info
     admin_username = azurerm_linux_virtual_machine.vm.admin_username
-    admin_password = azurerm_linux_virtual_machine.vm.admin_password
+    # admin_password = azurerm_linux_virtual_machine.vm.admin_password
   }
 }
