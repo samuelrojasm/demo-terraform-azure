6293bf48d06e90cb620e589dabe943a1c44275c3
srojas <srojas@192.168.1.25>
2025-04-21 17:10:12 -0600

    vm-same-image

--- virtual-machines/vm-same-image/outputs.tf
+++ virtual-machines/vm-same-image/outputs.tf
@@ -1,14 +1,12 @@
-
+# Datos presentados al final de Terraform apply
 
 output "resource_group_name" {
   value = azurerm_resource_group.rg.name
 }
 
-output "private_ip_address" {
-  value = azurerm_linux_virtual_machine.vm.private_ip_address
-}
-
-output "admin_password" {
-  sensitive = true
-  value     = azurerm_linux_virtual_machine.vm.admin_password
+output "Red" {
+  value = {
+    vnet_name   = try(azurerm_virtual_network.vnet.name, null)
+    subnet_name = try(azurerm_subnet.subnet.name, null)
+  }
 }
