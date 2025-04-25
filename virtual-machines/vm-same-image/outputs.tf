9c71814ae12705504872e9fc152e474825c576b4
srojas <srojas@192.168.1.25>
2025-04-25 12:02:42 -0600

    vm-same-image

--- virtual-machines/vm-same-image/outputs.tf
+++ virtual-machines/vm-same-image/outputs.tf
@@ -5,23 +5,32 @@ output "vm_count" {
   value       = length(azurerm_linux_virtual_machine.vm)
 }
 
+output "resource_group_name" {
+  description = "Nombre de Resource Group"
+  value       = azurerm_resource_group.rg.name
+}
+
 output "vm_info" {
   description = "Nombre de las VMs creadas"
   value = {
     nombre         = azurerm_linux_virtual_machine.vm[*].name
     public_ip      = azurerm_public_ip.public_ip[*].ip_address
     public_ip_name = azurerm_public_ip.public_ip[*].name
+    managed_identity_type = azurerm_linux_virtual_machine.vm[*].identity[0].type # Managed Identity (System Assigned)
   }
-}
-
-output "resource_group_name" {
-  description = "Nombre de Resource Group"
-  value       = azurerm_resource_group.rg.name
 }
 
 output "Red" {
   value = {
     vnet_name   = try(azurerm_virtual_network.vnet.name, null)
     subnet_name = try(azurerm_subnet.subnet.name, null)
+  }
+}
+
+output "Extensiones" {
+  value = {
+    aad_ssh_extension_name      = try(azurerm_virtual_machine_extension.aad_login.name, null)
+    aad_ssh_extension_type      = try(azurerm_virtual_machine_extension.aad_login.type, null)
+    aad_ssh_extension_publisher = try(azurerm_virtual_machine_extension.aad_login.publisher, null)
   }
 }
