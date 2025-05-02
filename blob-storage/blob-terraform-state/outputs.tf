28c5ea9d887ae03e2f34767828f37c0976e8f010
srojas <srojas@MacBook-Pro-de-srojas.local>
2025-05-01 18:19:55 -0600

    blob-terraform-state

--- blob-storage/blob-terraform-state/outputs.tf
+++ blob-storage/blob-terraform-state/outputs.tf
@@ -3,6 +3,9 @@
 # Outputs: Datos presentados al final de Terraform apply
 ##--------------------------------------------------------------
 
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
 output "storage_account_name" {
   value = azurerm_storage_account.storageacct.name
 }
